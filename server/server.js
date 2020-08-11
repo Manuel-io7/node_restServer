3bf28ea40717b810fbbb0437f650ccf007b2cb57
Manuel-io7 <z.zwats00@gmail.com>
2020-08-11 16:25:42 -0500

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 require ('./config/config');
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
+
+const bodyParser  = require ('body-parser'); 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
+
+app.use(require('./routes/usuario'));
+mongoose.connect(process.env.URLDB, (err,res) =>{
+  if (err) throw err; 
+    console.log('BD Online');
+  
+});
 app.listen(process.env.PORT, () => {
     console.log('escuchando puerto')
 });
