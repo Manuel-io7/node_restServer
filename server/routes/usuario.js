c5000321a62a76f7e767b80ac1cdd6c5781771d7
Manuel-io7 <z.zwats00@gmail.com>
2020-08-18 14:49:58 -0500

    Login

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const Usuario = require('../models/usuario');
+const {verificaToken,verificaAdmin_Role} = require('../middlewares/autenticacion');
 const app = express();
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken,  (req, res) =>{
+    return res.json({
+        usuario: req.usuario,
+        nombre: req.usuario.nombre,
+        email: req.usuario.email 
+    });
 
 
     let desde = req.query.desde || 0;
@@ -35,7 +41,7 @@ app.get('/usuario', function (req, res) {
     // res.json('get Usuario local');
   });
 
-app.post('/usuario', function(req,res){
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req,res){
     let body = req.body;
     
     let usuario = new Usuario({
@@ -63,7 +69,7 @@ app.post('/usuario', function(req,res){
 
 });
 //PUT
-app.put('/usuario/:id', function(req, res){
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res){
     let id = req.params.id;
     let body = _.pick(req.body,['nombre', 'email','img','role','estado']) ;
 
@@ -82,8 +88,8 @@ app.put('/usuario/:id', function(req, res){
     })
 
 });
-
-app.delete('/usuario/:id', function(req, res){
+//DELETE
+app.delete('/usuario/:id', [verificaToken,verificaAdmin_Role], function(req, res){
    let id = req.params.id;
 
   // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) =>{
--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
  
 
-app.use(require('./routes/usuario'));
+//configuracion de rutas
+app.use(require('./routes/index'));
+
 mongoose.connect(process.env.URLDB, (err,res) =>{
   if (err) throw err; 
     console.log('BD Online');
